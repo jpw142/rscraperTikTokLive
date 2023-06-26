6ec272f9c36fa62239185810d62a28d81c2ca8e8
Jack Weber <jackweber142@gmail.com>
2023-06-26 02:25:58 -0400

    fixed match nastiness shenanigans

--- src/main.rs
+++ src/main.rs
@@ -28,46 +28,41 @@ use tokio::{self, time::sleep};
     d.goto(url).await?;
     // Checks if the login screen comes up
     let login = d.find(By::Css(".tiktok-aiuhe9-DivModalContent")).await;
-    match login{
+    if let Ok(_) = login {
         // If found we should login
-        Ok(_) => {
-            // Wait for sign in screen and click sign in with email
-            let sb = d.query(By::Css("div.tiktok-2pt368-DivBoxContainer:nth-child(5)")).first().await?;
-            sb.wait_until().displayed().await?;
-            sb.click().await?;
-            sleep(Duration::from_millis(2500)).await;
-            
-            // Get list of windows and switch to login window, empiracally I've found the second window is the login, this may be wrong
-            let windows = d.windows().await?;
-            d.switch_to_window(windows[1].clone()).await?;
-            sleep(Duration::from_millis(1500)).await;
+        // Wait for sign in screen and click sign in with email
+        let sb = d.query(By::Css("div.tiktok-2pt368-DivBoxContainer:nth-child(5)")).first().await?;
+        sb.wait_until().displayed().await?;
+        sb.click().await?;
+        sleep(Duration::from_millis(2500)).await;
+        
+        // Get list of windows and switch to login window, empiracally I've found the second window is the login, this may be wrong
+        let windows = d.windows().await?;
+        d.switch_to_window(windows[1].clone()).await?;
+        sleep(Duration::from_millis(1500)).await;
 
-            // If we already have our email in then click it else
-            let prelog = d.find(By::Css(".tgnCOd")).await;
-            match prelog {
-                Ok(element) => {
-                    element.click().await?;
-                }
-                Err(_) => {
-                    // Enter email into email box
-                    let ee = d.query(By::Css("#identifierId")).first().await?;
-                    ee.send_keys(email).await?;
-                    // Go to next page
-                    let next = d.query(By::Css(".VfPpkd-LgbsSe-OWXEXe-k8QpJ > span:nth-child(4)")).first().await?;
-                    next.click().await?;
-                    sleep(Duration::from_millis(2000)).await;
-                    // Enter password into password box
-                    let ep = d.query(By::Css("#password > div.aCsJod.oJeWuf > div > div.Xb9hP > input")).first().await?;
-                    ep.send_keys(password).await?;
-                    // Go to next page
-                    let next2 = d.query(By::Css(".VfPpkd-LgbsSe-OWXEXe-k8QpJ > span:nth-child(4)")).first().await?;
-                    next2.click().await?;
-                    sleep(Duration::from_millis(5000)).await;
-                    // By this point it's loading and logged in successfully
-                }
-            }
+        // If we already have our email in then click it else
+        let prelog = d.find(By::Css(".tgnCOd")).await;
+        if let Ok(element) = prelog {
+            element.click().await?;
         }
-        Err(_) => ()
+        else {
+            // Enter email into email box
+            let ee = d.query(By::Css("#identifierId")).first().await?;
+            ee.send_keys(email).await?;
+            // Go to next page
+            let next = d.query(By::Css(".VfPpkd-LgbsSe-OWXEXe-k8QpJ > span:nth-child(4)")).first().await?;
+            next.click().await?;
+            sleep(Duration::from_millis(2000)).await;
+            // Enter password into password box
+            let ep = d.query(By::Css("#password > div.aCsJod.oJeWuf > div > div.Xb9hP > input")).first().await?;
+            ep.send_keys(password).await?;
+            // Go to next page
+            let next2 = d.query(By::Css(".VfPpkd-LgbsSe-OWXEXe-k8QpJ > span:nth-child(4)")).first().await?;
+            next2.click().await?;
+            sleep(Duration::from_millis(5000)).await;
+            // By this point it's loading and logged in successfully
+        }
     }
     Ok(())
 }
